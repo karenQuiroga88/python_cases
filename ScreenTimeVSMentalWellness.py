39dad5fbb5db61bac320bd267086cfeb17ae13e4
karenQuiroga88 <115047409+karenQuiroga88@users.noreply.github.com>
2025-10-12 10:04:52 -0300

    Update ScreenTimeVSMentalWellness.py

--- ScreenTimeVSMentalWellness.py
+++ ScreenTimeVSMentalWellness.py
@@ -1,4 +1,6 @@
 ### Screen Time vs Mental Wellness Survey
+###Tratar a base de dados, excluindo valores nulos, ajustando duplicidades conforme necessário 
+# e realizando o levantamento de métricas gerais, salvando os dados tratados em csv.
 
 import pandas as pd 
 import os
@@ -9,35 +11,66 @@ caminhoCompleto = os.path.join(caminho,arquivo)
 
 # ler arquivo 
 df = pd.read_csv(caminhoCompleto)
-##print(df.head(100))
 
-##print (df.columns.tolist())
+#print(df.head (10))
+
+# print (df.columns.tolist())
+
 
  # ----------------------------------------------- LIMBEZA DE DADOS -------------------------------------------------------------
 "Verificando se extiste linhas com dados nulos por coluna"
 print(df.isnull().sum())
 
+print(df.columns)
 
-"Removendo linhas somente com valores nulos em linhas espefícicas, caso necessário"
+# Aqui exclui a ultima coluna que estava em branco
+df = df.drop(columns=['Unnamed: 15'])
 
-df_semNulos = df.dropna(subset=['age', 'gender', 'user_id', 'screen_time_hours'])
+print(df.isnull().sum())
 
-linhasAntes = df.shape[0]
-linhasDepois = df_semNulos.shape[0]
-
-print (f' Total de {linhasAntes} linhas antes')
-print (f' Total de {linhasDepois} linhas depois')
-
-if linhasAntes > linhasDepois: 
-    print(f'Total de {linhasAntes -linhasDepois} linhas excluídas')
-else:
-    print ('Nenhuma linha excluída')
-
+##print(list(df.columns)) 
 
 " Verificnado se existem dados duplicados"
 
 print(df.duplicated()) # exibindo os itens duplicados, caso exista.
 print (f'numero de linhas duplicadas: {df.duplicated().sum()}')
 
+
 # ----------------------------------------------- METRICAS GERAIS ----------------------------------------------------------------------
 
+# Contar nmúmero de users por age e gender
+df_countUser = df.groupby(['age','gender'])['user_id'].nunique()
+print (df_countUser)
+
+# Contar múmero total e distintos de users por age e gender
+df_countUserAll = df.groupby(['age','gender']). agg({'user_id':['nunique','count']})
+print (df_countUserAll)
+
+
+df_resultado = df.groupby(['occupation','work_mode','gender']). agg({'user_id': 'nunique',
+                                                                      'age': 'median',
+                                                                      'screen_time_hours': 'median',
+                                                                      'work_screen_hours': 'median',
+                                                                      'leisure_screen_hours': 'median',
+                                                                      'sleep_hours': 'median',
+                                                                      'exercise_minutes_per_week':'median',
+                                                                      'stress_level_0_10':'median',
+                                                                      'productivity_0_100':'median', 
+                                                                      'social_hours_per_week': 'median',
+                                                                      'mental_wellness_index_0_100': 'median' }).reset_index().round(0)
+print (df_resultado)
+
+df_resultado = pd.DataFrame(df_resultado)
+
+df_resultado.to_csv('resultado_consolidado.csv', index = False)
+print('Dados salvos em csv')
+
+df.to_csv('raw.csv', index = False)
+print('Dados salvos em csv')
+
+"""
+Pefuntas as serem respondidas por gráfico
+- Média produtividade por tipo de work_mode ( filtrando somente pela occuppation "employed")
+- Fazer a comparação acima por genero 
+- Entender media_social_hours por week impacta na na produtividade e mental well ness em cada um dos tipos de work mode.
+"""
